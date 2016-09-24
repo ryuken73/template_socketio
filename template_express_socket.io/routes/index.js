0a29bc1306f29dd4e5d5392c5978ecace7c21883
ryuken73 <ryuken73@gmail.com>
2016-09-24 14:42:29 +0900

    init

--- template_express_socket.io/routes/index.js
+++ template_express_socket.io/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* change in master */
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express 1' });
+  res.render('index', { title: 'Express 1' }); 
 });
 
 module.exports = router;      
