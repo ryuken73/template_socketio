4d82aa3ad17c850e0f123b855a801e1ed6b71f95
ryuken73 <ryuken73@gmail.com>
2016-12-03 20:41:19 +0900

    add detail

--- template_express_socket.io/public/js/admin.js
+++ template_express_socket.io/public/js/admin.js
@@ -9,34 +9,99 @@ $(document).ready(function(){
 function handleConnect(socket){
 	socket.on('connect',function(){
 		console.log('connected');
+		$('#connection_status').text('connected');
+		$('#connection_status').attr('class','connected');
 		handleDisconnect(socket);
-        handleRefesh(socket)
+        handleSummaryRefesh(socket);
+        handleDetailRefresh(socket);
 	})
 }
 
 function handleDisconnect(socket){
 	socket.on('disconnect',function(){
 		console.log('disconnected');
+		$('#connection_status').attr('class','disconnected');
 	})
 }
 
-function handleRefesh(socket){
-	socket.on('server-to-client init',function(data){
-		socket.emit('client-to-server reqStatus');		
+function handleDetailRefresh(socket){
+	socket.on('adminDetail-server-to-client init',function(data){
+		socket.emit('client-to-server reqAllStatus');
 	});
 	
-	socket.on('server-to-client resStatus',function(data){		
+	socket.on('server-to-client resAllStatus', function(data){	
+		updateDetail(data);
+		addDetail(data);
+		socket.emit('client-to-server resAllStatusDone');	
+	});
+	
+}
+
+function addDetail(data){
+	
+	var newData = _.filter(data, function(socket){
+		var socketID = socket.socketID;
+		return !(_.some($('.socket'),['id', socketID]));
+	});
+	console.log(newData);
+	newData.forEach(function(socket){
+		console.log('append : ' + socket.socketID);
+		var sockData = '<div id=' + socket.socketID + ' class = "row socket">';
+		sockData    +=   '<div id=roomNM class="three columns">' + socket.roomNM + '</div>';
+		sockData    +=   '<div id=addr class="two columns">' + socket.remoteAddr + '</div>';
+		sockData    +=   '<div id=ctime class="two columns">' + socket.clientTime + '</div>';		
+		sockData    +=   '<div id=stime class="two columns">' + socket.serverTime + '</div>';		
+		sockData    +=   '<div id=offset class="two columns">' + socket.tMonOffset + '</div>';	
+		sockData    +=   '<div id=status class="one columns">' + socket.tMonStatus + '</div>';			
+		
+		sockData     += '</div>  ';
+		$('#detail').append(sockData);
+	
+	});
+	
+}
+
+function updateDetail(data){
+	// update some rows which is in newly arrived data
+	var updateData = _.filter(data, function(socket){
+		var socketID = socket.socketID;
+		return _.some($('.socket'),['id', socketID]);
+	})
+	updateData.forEach(function(socket){
+		console.log('update : ' + socket.socketID);
+		var escapedID = socket.socketID.replace( /(:|\.|\/|\#|\-|\[|_|\]|,|=)/g, "\\$1" );
+		console.log('escaped : ' + escapedID);
+		$('#detail #'+escapedID+' #roomNM').text(socket.roomNM);
+		$('#detail #'+escapedID+' #addr').text(socket.remoteAddr);
+		$('#detail #'+escapedID+' #ctime').text(socket.clientTime);
+		$('#detail #'+escapedID+' #stime').text(socket.serverTime);
+		$('#detail #'+escapedID+' #offset').text(socket.tMonOffset);
+		$('#detail #'+escapedID+' #status').text(socket.tMonStatus);
+
+		
+	}) 	
+	
+}
+
+
+function handleSummaryRefesh(socket){
+	socket.on('adminSummary-server-to-client init',function(data){
+		socket.emit('client-to-server reqSummary');		
+	});
+	
+	socket.on('server-to-client resSummary',function(data){		
 		removeData(data);
 		updateData(data);
 		addData(data);
 		chgHeader(data);
-		socket.emit('client-to-server resDone');
+		socket.emit('client-to-server resSummaryDone');
 
 	})
 }   
 
 function removeData(data){
 
+	// remove some previous rows which is not in newly arrived data 
 	var noData = _.reject($('.group') ,function(element){
 		var roomNM = element.id;
 		return _.some(data,['roomNM',roomNM]);
@@ -48,6 +113,8 @@ function removeData(data){
 }
 
 function addData(data){
+	
+	// add new rows which is newly included in arrived data
 	var newData = _.filter(data, function(group){
 		var roomNM = group.roomNM;
 		return !(_.some($('.group'),['id', roomNM]));
@@ -66,7 +133,19 @@ function addData(data){
 	})
 }
 
+function mkSatusHtml(status){
+	var numGood = status.GOOD ? status.GOOD:0;
+	var numWarn = status.WARN ? status.WARN:0;
+	var numFail = status.FAIL ? status.FAIL:0;
+	
+	var good = '<span class="good">Good:' + numGood + ' </span>';
+	var warn = '<span class="warn">Warn:' + numWarn + ' </span>';
+	var fail = '<span class="fail">Fail:' + numFail + ' </span>';
+	return good + ' / ' + warn + ' / ' + fail;
+}
+
 function updateData(data){
+	// update some rows which is in newly arrived data
 	var updateData = _.filter(data, function(group){
 		var roomNM = group.roomNM;
 		return _.some($('.group'),['id', roomNM]);
@@ -80,16 +159,6 @@ function updateData(data){
 	}) 
 }
 
-function mkSatusHtml(status){
-	var numGood = status.GOOD ? status.GOOD:0;
-	var numWarn = status.WARN ? status.WARN:0;
-	var numFail = status.FAIL ? status.FAIL:0;
-	
-	var good = '<span class="good">Good:' + numGood + ' </span>';
-	var warn = '<span class="warn">Warn:' + numWarn + ' </span>';
-	var fail = '<span class="fail">Fail:' + numFail + ' </span>';
-	return good + ' / ' + warn + ' / ' + fail;
-}
 
 function chgHeader(data){
 	// change total Connected
