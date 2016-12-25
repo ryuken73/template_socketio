b4071779f4f1b7305933c416e857afafee0b89e2
ryuken73 <ryuken73@gmail.com>
2016-12-25 20:45:06 +0900

    add function - electron client can read local json config file

--- template_express_socket.io/public/js/index.js
+++ template_express_socket.io/public/js/index.js
@@ -1,6 +1,48 @@
 // client code
 
 $(document).ready(function(){
+	
+    console.log('ready');
+
+    if(window.nodeRequire){
+    	// Electron Client
+    	console.log('electron env');
+    	// retrun previously changed "require"
+	    var require = window.nodeRequire;
+	    // use node.js API
+    	var fs = require('fs');
+    	var path = require('path');
+    	var url = require('url');
+    	// process current execution directory
+    	// console.log(process.cwd());	
+    	
+    	// read local config file using require
+    	var configFile = path.resolve(path.join(process.cwd(), 'config.json'));
+    	var config = require(configFile);
+    	$('#status').append('<div> Server : ' + config.url + '</div>');
+    	$('#status').append('<div> Group : ' + config.groupNM + '</div>');
+
+    	// read local config file using node fs module
+    	var configObj = JSON.parse(fs.readFileSync(process.cwd() + '/config.json'));
+     	$('h1').append('<div>' + configObj.name + '</div>');  
+    }else{
+    	// Browser Client
+    	console.log('browser env');
+    }
+
+	/*
+	if(env === 'electron'){
+		// electron client
+		//config = remote.require('./config.json');
+		//console.log(config.url);
+		//console.log(config.groupNM);
+		console.log('electron client');
+	}else{
+		// web browser client
+		console.log('this is browser client ( not electron )');
+	}
+	*/
+	
 	var socket = io();
 	handleConnect(socket);
 	haneleTime(socket);
